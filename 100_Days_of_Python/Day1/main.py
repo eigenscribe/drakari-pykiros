667f176539a12dce68b5cdf1eb7a20812f9cd99f
eigenscribe <111261353+eigenscribe@users.noreply.github.com>
2024-09-24 20:04:41 -0400

    Add files via upload

---
+++ 100_Days_of_Python/Day1/main.py
@@ -0,0 +1,13 @@
+print("Lauren Shriver")
+print("June 11, 2023")
+
+print()
+
+print("""I am signing up for Replit's 100 days of Python challenge! I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day. I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+
+print()
+
+print("I am feeling 💗")
+print()
+
+print("You can follow my progress at replit.com/@eigenscribe")
